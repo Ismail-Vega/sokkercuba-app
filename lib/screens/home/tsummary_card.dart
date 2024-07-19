c60e923c386c8a5079671728b37fd2e2e9e5ed19
Ismail Vega <ismail.softing@gmail.com>
2024-07-19 16:12:45 -0300

    fix(Home): fix issues in home screen
    Signed-off-by: Ismail Vega <ismail.softing@gmail.com>

--- lib/screens/home/tsummary_card.dart
+++ lib/screens/home/tsummary_card.dart
@@ -18,10 +18,6 @@ class TSummaryCard extends StatelessWidget {
 
     List<Week> weeks = tsummary.weeks;
 
-    if (weeks.isNotEmpty && weeks[0].gameDay.day != 1) {
-      weeks = weeks.sublist(1);
-    }
-
     return Card(
       color: Colors.blueAccent,
       child: Padding(
@@ -39,147 +35,157 @@ class TSummaryCard extends StatelessWidget {
                       )),
                 ]),
             const SizedBox(height: 8),
-            Table(
-              columnWidths: const {
-                0: FlexColumnWidth(1.5),
-                1: FlexColumnWidth(2),
-                2: FlexColumnWidth(2),
-              },
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: [
-                TableRow(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12.0),
-                      topRight: Radius.circular(12.0),
-                    ),
-                  ),
+            SizedBox(
+              height: 324.0, // Adjust the height as needed
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical, // Allows vertical scrolling
+                child: Table(
+                  columnWidths: const {
+                    0: FlexColumnWidth(1.5),
+                    1: FlexColumnWidth(2),
+                    2: FlexColumnWidth(2),
+                  },
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                   children: [
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Center(child: Text("")),
-                      ),
-                    ),
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Center(
-                            child:
-                                Text("MAIN TEAM", textAlign: TextAlign.center)),
-                      ),
-                    ),
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Center(
-                            child:
-                                Text("JUNIORS", textAlign: TextAlign.center)),
-                      ),
-                    ),
-                  ],
-                ),
-                const TableRow(
-                  children: [
-                    TableCell(
-                      child: SizedBox(height: 8.0),
-                    ),
-                    TableCell(
-                      child: SizedBox(height: 8.0),
-                    ),
-                    TableCell(
-                      child: SizedBox(height: 8.0),
-                    ),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Center(child: Text("Date")),
-                      ),
-                    ),
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(child: Center(child: Text("No."))),
-                            Expanded(child: Center(child: Text("Skills Up"))),
-                          ],
+                    TableRow(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12.0),
+                          topRight: Radius.circular(12.0),
                         ),
                       ),
-                    ),
-                    TableCell(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(child: Center(child: Text("No."))),
-                            Expanded(child: Center(child: Text("Levels Up"))),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                ...weeks.asMap().entries.map((entry) {
-                  final index = entry.key;
-                  final week = entry.value;
-                  return TableRow(
-                    decoration: index != weeks.length - 1
-                        ? BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                                    color: Colors.blue[900] ?? Colors.blue)),
-                          )
-                        : null,
-                    children: [
-                      TableCell(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Center(
-                            child: Text(week.gameDay.date.value),
+                      children: [
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Center(child: Text("")),
                           ),
                         ),
-                      ),
-                      TableCell(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                                child: Center(
-                                    child:
-                                        Text(week.stats.advanced.toString()))),
-                            Expanded(
-                                child: Center(
-                                    child:
-                                        Text(week.stats.skillsUp.toString()))),
-                          ],
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Center(
+                                child: Text("MAIN TEAM",
+                                    textAlign: TextAlign.center)),
+                          ),
                         ),
-                      ),
-                      TableCell(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                                child: Center(
-                                    child:
-                                        Text(week.juniors.number.toString()))),
-                            Expanded(
-                                child: Center(
-                                    child: Text(
-                                        week.juniors.skillsUp.toString()))),
-                          ],
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Center(
+                                child: Text("JUNIORS",
+                                    textAlign: TextAlign.center)),
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                }),
-              ],
+                      ],
+                    ),
+                    const TableRow(
+                      children: [
+                        TableCell(
+                          child: SizedBox(height: 8.0),
+                        ),
+                        TableCell(
+                          child: SizedBox(height: 8.0),
+                        ),
+                        TableCell(
+                          child: SizedBox(height: 8.0),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Center(child: Text("Date")),
+                          ),
+                        ),
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(child: Center(child: Text("No."))),
+                                Expanded(
+                                    child: Center(child: Text("Skills Up"))),
+                              ],
+                            ),
+                          ),
+                        ),
+                        TableCell(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(child: Center(child: Text("No."))),
+                                Expanded(
+                                    child: Center(child: Text("Levels Up"))),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ...weeks.asMap().entries.map((entry) {
+                      final index = entry.key;
+                      final week = entry.value;
+                      return TableRow(
+                        decoration: index != weeks.length - 1
+                            ? BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Colors.blue[900] ?? Colors.blue)),
+                              )
+                            : null,
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Center(
+                                child: Text(week.gameDay.date.value),
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                    child: Center(
+                                        child: Text(
+                                            week.stats.advanced.toString()))),
+                                Expanded(
+                                    child: Center(
+                                        child: Text(
+                                            week.stats.skillsUp.toString()))),
+                              ],
+                            ),
+                          ),
+                          TableCell(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                    child: Center(
+                                        child: Text(
+                                            week.juniors.number.toString()))),
+                                Expanded(
+                                    child: Center(
+                                        child: Text(
+                                            week.juniors.skillsUp.toString()))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
--- lib/screens/home/welcome_screen.dart
+++ lib/screens/home/welcome_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../models/team/user.dart';
+import '../../state/actions.dart';
+import '../../state/app_state_notifier.dart';
 import 'squad_summary_card.dart';
 import 'tsummary_card.dart';
 import 'user_card.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
+  final User? user;
 
-  const WelcomeScreen({super.key});
+  const WelcomeScreen({super.key, this.user});
 
   @override
   State<WelcomeScreen> createState() {
@@ -38,6 +43,34 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      handleUserUpdate();
+    });
+  }
+
+  void handleUserUpdate() {
+    if (widget.user != null) {
+      final week = widget.user?.today.week;
+      final day = widget.user?.today.day;
+      final appStateNotifier =
+          Provider.of<AppStateNotifier>(context, listen: false);
+      final stateWeek = appStateNotifier.state.trainingWeek;
+
+      if (week != null && day != null) {
+        final trainingWeek = day < 5 ? week - 1 : week;
+
+        if (trainingWeek != stateWeek) {
+          appStateNotifier.dispatch(
+              StoreAction(StoreActionTypes.setTrainingWeek, trainingWeek));
+        }
+      }
+    }
   }
 
   @override
